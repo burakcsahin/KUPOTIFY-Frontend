8eedae789781722655c421c7831c924ab2f58ace
Bkurtuldu <91678995+Bkurtuldu@users.noreply.github.com>
2024-01-21 02:41:32 +0300

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -17,8 +17,8 @@ class Server {
         this.connection = mysql.createConnection({
             host: 'localhost',
             user: 'root',
-            password: 'MBesbardam123.',
-            database: 'comp306',
+            password: 'berke61752',
+            database: '306schema',
         });
 
         // Initialize routes
@@ -36,6 +36,8 @@ class Server {
             const songNameToInsert = req.query.songName;
             const songGenreToInsert = req.query.genreToInsert;
             const songID = req.query.songID;
+            const artistNameToFilter = req.query.artistNameToFilter;
+            const albumID = req.query.albumID;
 
             let run_query = null;
 
@@ -131,7 +133,7 @@ class Server {
                 album.albID = song_inside_album.albID and
                 song.songID = song_inside_album.songID`;
 
-                
+
                 if (!artistName.trim() == "") {
                     run_query += ` and name = "${artistName}"`;
                 }
@@ -143,23 +145,50 @@ class Server {
                 run_query += `;`;
             }
 
-            else if(number == 13){
+            else if (number == 13) {
                 run_query = `select distinct albName from album order by albName;`;
 
             }
 
-            else if(number == 14){
+            else if (number == 14) {
                 run_query = `insert into song(songID,songName,songCategory) values(${songID}, "${songNameToInsert}", "${songGenreToInsert}")`;
             }
-            
-            else if(number == 15){
+
+            else if (number == 15) {
                 run_query = `select max(songID) + 1 from song;`;
             }
 
-            else if(number == 16){
-                run_query = `select albID from Album where albName = "${albName}"`;
+            else if (number == 16) {
+                run_query = `select * 
+                             from album, artist
+                             where artist.artistID = album.artistID and name = "${artistNameToFilter}" and albName = "${albName}";`;
+            }
+            else if (number == 17) {
+                run_query = `select distinct name from artist;`;
             }
 
+            else if (number == 18) {
+                run_query = `select albName
+                             from album, artist
+                             where album.artistID = artist.artistID and
+                             artist.name = "${artistNameToFilter}";`;
+            }
+
+            else if (number == 19) {
+                run_query = `insert into song_inside_album values("${albumID}",${songID});`;
+            }
+
+            else if (number == 20) {
+                run_query = `select songName from song where songID = ${songID};`;
+            }
+
+            else if (number == 21) {
+                run_query = `delete from song_inside_album where songID = ${songID};`;
+            }
+
+            else if (number == 22) {
+                run_query = `delete from song where songID = ${songID};`;
+            }
 
 
 
